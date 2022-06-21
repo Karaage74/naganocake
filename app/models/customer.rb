9912175641426bbc1bfe8a3201ab86b8a10a4685
takuori <104710179+takuori@users.noreply.github.com>
2022-06-21 04:11:33 +0000

    退会/配送先一覧・編集/会員情報編集機能追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,12 +3,21 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  def full_name
-    self.last_name + "" + self.first_name
-  end
   
+  has_many :cart_items
+  has_many :shipping_addresses
+  has_many :orders
+
+  def full_name
+    self.last_name + self.first_name
+  end
+
   def kana_full_name
     self.last_name_kana + "" + self.first_name_kana
   end
+
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
+
 end
