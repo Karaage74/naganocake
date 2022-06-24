29aaf2b9ffd5824cf894ab7b95b2e8deb92ff4f9
suzu784 <s.suzuki784@hotmail.com>
2022-06-24 03:43:41 +0000

    会員側と管理者側のログイン後のパス変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def after_sign_out_path_for(resource)
-    new_customer_session_path
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :member
+      new_customer_session_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
 
   def configure_permitted_parameters
