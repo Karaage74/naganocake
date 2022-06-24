97f23f248a4a1f16d53ad34fd3bfbece0bb8a96f
EC2 Default User <ec2-user@ip-172-31-22-225.ec2.internal>
2022-06-24 21:04:50 +0000

    いいね機能追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,14 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items
   has_many :order_details
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_customer, through: :favorites, source: :customer
 
   has_one_attached :item_image
+
+  def favorited_by?(customer)
+    favorites.where(customer_id: customer.id).exists?
+  end
 
   def with_tax_price
     (price_without_tax * 1.1).floor
