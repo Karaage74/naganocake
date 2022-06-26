02fadd75be42554b3d749c5dfab733d494293111
suzu784 <s.suzuki784@hotmail.com>
2022-06-26 13:45:40 +0000

    画像サイズ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,14 @@ class Item < ApplicationRecord
   has_many :item_comments, dependent: :destroy
 
   has_one_attached :item_image
+  
+  def get_item_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit:[width, height]).processed
+  end
 
   def favorited_by?(customer)
     favorites.where(customer_id: customer.id).exists?
